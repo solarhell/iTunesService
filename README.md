314e4429a7c7b4b91e1cd840125af53fe79e4914
solarhell <songjiaxin2008@gmail.com>
2019-08-06 17:03:57 +0800

    Init.

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # iTunesService
+
+## usage
+
+```go
+package main
+
+import (
+	"context"
+	"errors"
+	"github.com/smallnest/rpcx/client"
+	"log"
+)
+
+func main()  {
+	d := client.NewPeer2PeerDiscovery("tcp@localhost:8972", "")
+	xclient := client.NewXClient("iTunes", client.Failtry, client.RandomSelect, d, client.DefaultOption)
+	defer xclient.Close()
+
+	args := &Args{
+		Name: "李志",
+	}
+
+	reply := &Reply{}
+	err := xclient.Call(context.Background(), "GetArtistPictureImageUrl", args, reply)
+	if err != nil {
+		log.Fatalf("failed to call: %v", err)
+	}
+
+	log.Println(reply.URL)
+}
+
+```
---
+++ main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"github.com/antchfx/htmlquery"
+	"github.com/imroc/req"
+	"github.com/smallnest/rpcx/server"
+	"net/url"
+	"strings"
+)
+
+type Args struct {
+	Name string
+}
+
+type Reply struct {
+	URL string
+}
+
+func GetArtistPictureImageUrl(ctx context.Context, args *Args, reply *Reply) error {
+	if args.Name == "" {
+		return errors.New("歌手名不能为空")
+	}
+	u, err := url.Parse("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/wa/wsSearch")
+	if err != nil {
+		return err
+	}
+
+	q := u.Query()
+	q.Add("country", "hk")
+	q.Add("entity", "allArtist")
+	q.Add("term", args.Name)
+	q.Add("limit", "1")
+
+	u.RawQuery = q.Encode()
+
+	r, err := req.Get(u.String())
+	if err != nil {
+		return err
+	}
+
+	type artistResponse struct {
+		ResultCount int `json:"resultCount"`
+		Results     []struct {
+			WrapperType      string `json:"wrapperType"`
+			ArtistType       string `json:"artistType"`
+			ArtistName       string `json:"artistName"`
+			ArtistLinkURL    string `json:"artistLinkUrl"`
+			ArtistID         int    `json:"artistId"`
+			PrimaryGenreName string `json:"primaryGenreName"`
+			PrimaryGenreID   int    `json:"primaryGenreId"`
+		} `json:"results"`
+	}
+
+	artist := artistResponse{}
+	err = r.ToJSON(&artist)
+	if err != nil {
+		return err
+	}
+
+	if len(artist.Results) == 0 {
+		return err
+	}
+
+	r, err = req.Get(artist.Results[0].ArtistLinkURL)
+	if err != nil {
+		return err
+	}
+
+	str, err := r.ToString()
+	if err != nil {
+		return err
+	}
+
+	doc, err := htmlquery.Parse(strings.NewReader(str))
+	if err != nil {
+		return err
+	}
+
+	imageEle := htmlquery.FindOne(doc, "//meta[@property='og:image']/@content")
+	imageStr := htmlquery.InnerText(imageEle)
+	imageProcessed := strings.Replace(imageStr, "1200x630cw.png", "99999x99999.png", 1)
+	reply.URL = imageProcessed
+	return nil
+}
+
+func main() {
+	s := server.NewServer()
+	var err error
+	err = s.RegisterFunctionName("iTunes", "GetArtistPictureImageUrl", GetArtistPictureImageUrl, "")
+	if err != nil {
+		panic(err)
+	}
+	err = s.Serve("tcp", ":8972")
+	if err != nil {
+		panic(err)
+	}
+}
